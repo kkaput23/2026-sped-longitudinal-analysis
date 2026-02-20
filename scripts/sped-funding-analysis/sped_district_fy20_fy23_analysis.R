20c8b907e7a434e1cc343d4405b57247b3ad045c
kkaput23 <kkaput23@gmail.com>
2026-02-20 14:30:07 -0600

    Update sped_district_fy20_fy23_analysis.R

--- scripts/sped-funding-analysis/sped_district_fy20_fy23_analysis.R
+++ scripts/sped-funding-analysis/sped_district_fy20_fy23_analysis.R
@@ -60,6 +60,65 @@ sped_district_fy20_fy23_cpi <- sped_exp_rev_enroll_join_cpi |>
          state_fed_sped_pp_funding_fy23, total_sped_pp_expenditure_fy23, local_sped_pp_obligation_fy20,
          local_sped_pp_obligation_fy23, local_sped_pp_obligation_diff)
 
+# District summary: Districts that saw an INCREASE in their local pp obligation: NOT adjusted for inflation  ----
+
+# This is 3199 of the districts out of 4008 
+district_grow_local_obligation  <- sped_district_fy20_fy23_no_cpi |>
+  filter(local_sped_pp_obligation_diff > 0) |>
+  summarise(state_sped_rev_fy20 = sum(state_sped_rev_fy20, na.rm = T),
+            federal_sped_rev_fy20 = sum(federal_sped_rev_fy20, na.rm = T),
+            state_fed_sped_rev_fy20 = sum(state_fed_sped_rev_fy20, na.rm = T),
+            current_sped_exp_fy20 = sum(current_sped_exp_fy20, na.rm = T),
+            sped_enroll_fy20 = sum(sped_enroll_fy20, na.rm = T),
+            state_sped_rev_fy23 = sum(state_sped_rev_fy23, na.rm = T),
+            federal_sped_rev_fy23 = sum(federal_sped_rev_fy23, na.rm = T),
+            state_fed_sped_rev_fy23 = sum(state_fed_sped_rev_fy23, na.rm = T),
+            current_sped_exp_fy23 = sum(current_sped_exp_fy23, na.rm = T),
+            sped_enroll_fy23 = sum(sped_enroll_fy23, na.rm = T)) |>
+  mutate(state_sped_pp_funding_fy20 = state_sped_rev_fy20 / sped_enroll_fy20,
+         federal_sped_pp_funding_fy20 = federal_sped_rev_fy20 / sped_enroll_fy20,
+         state_fed_sped_pp_funding_fy20 = state_fed_sped_rev_fy20 / sped_enroll_fy20, 
+         total_sped_pp_expenditure_fy20 =  current_sped_exp_fy20 / sped_enroll_fy20, 
+         state_sped_pp_funding_fy23 = state_sped_rev_fy23 / sped_enroll_fy23,
+         federal_sped_pp_funding_fy23 = federal_sped_rev_fy23 / sped_enroll_fy23,
+         state_fed_sped_pp_funding_fy23 = state_fed_sped_rev_fy23 / sped_enroll_fy23, 
+         total_sped_pp_expenditure_fy23 =  current_sped_exp_fy23 / sped_enroll_fy23) |>
+  # Create the sped local calculation 
+  mutate(local_sped_pp_obligation_fy20 = total_sped_pp_expenditure_fy20 - state_fed_sped_pp_funding_fy20,
+         local_sped_pp_obligation_fy23 = total_sped_pp_expenditure_fy23 - state_fed_sped_pp_funding_fy23,
+         local_sped_pp_obligation_diff = local_sped_pp_obligation_fy23 - local_sped_pp_obligation_fy20) 
+  
+
+# District summary: Districts that saw a DECLINE in their lcoal pp obligation: NOT adjusted for inflation  ----
+
+# This is 801 of the districts out of 4008 
+district_decline_local_obligation  <- sped_district_fy20_fy23_no_cpi |>
+  filter(local_sped_pp_obligation_diff < 0) |>
+  summarise(state_sped_rev_fy20 = sum(state_sped_rev_fy20, na.rm = T),
+            federal_sped_rev_fy20 = sum(federal_sped_rev_fy20, na.rm = T),
+            state_fed_sped_rev_fy20 = sum(state_fed_sped_rev_fy20, na.rm = T),
+            current_sped_exp_fy20 = sum(current_sped_exp_fy20, na.rm = T),
+            sped_enroll_fy20 = sum(sped_enroll_fy20, na.rm = T),
+            state_sped_rev_fy23 = sum(state_sped_rev_fy23, na.rm = T),
+            federal_sped_rev_fy23 = sum(federal_sped_rev_fy23, na.rm = T),
+            state_fed_sped_rev_fy23 = sum(state_fed_sped_rev_fy23, na.rm = T),
+            current_sped_exp_fy23 = sum(current_sped_exp_fy23, na.rm = T),
+            sped_enroll_fy23 = sum(sped_enroll_fy23, na.rm = T)) |>
+  mutate(state_sped_pp_funding_fy20 = state_sped_rev_fy20 / sped_enroll_fy20,
+         federal_sped_pp_funding_fy20 = federal_sped_rev_fy20 / sped_enroll_fy20,
+         state_fed_sped_pp_funding_fy20 = state_fed_sped_rev_fy20 / sped_enroll_fy20, 
+         total_sped_pp_expenditure_fy20 =  current_sped_exp_fy20 / sped_enroll_fy20, 
+         state_sped_pp_funding_fy23 = state_sped_rev_fy23 / sped_enroll_fy23,
+         federal_sped_pp_funding_fy23 = federal_sped_rev_fy23 / sped_enroll_fy23,
+         state_fed_sped_pp_funding_fy23 = state_fed_sped_rev_fy23 / sped_enroll_fy23, 
+         total_sped_pp_expenditure_fy23 =  current_sped_exp_fy23 / sped_enroll_fy23) |>
+  # Create the sped local calculation 
+  mutate(local_sped_pp_obligation_fy20 = total_sped_pp_expenditure_fy20 - state_fed_sped_pp_funding_fy20,
+         local_sped_pp_obligation_fy23 = total_sped_pp_expenditure_fy23 - state_fed_sped_pp_funding_fy23,
+         local_sped_pp_obligation_diff = local_sped_pp_obligation_fy23 - local_sped_pp_obligation_fy20) 
+
+
+
 # Export the district data ------
 
 write_csv(sped_district_fy20_fy23_no_cpi, "sped-data/processed/district-analysis/sped_district_fy20_fy23_no_cpi.csv")
